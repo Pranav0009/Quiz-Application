bde01167750b43fd17becb2df249850611d078fd
Pranav0009 <gpranav0003@gmail.com>
2023-08-09 23:01:36 +0530

    final

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,29 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which symbol is used to refer the contents of a variable?",
+            "Double dot (..) with cd refers to?",
+            "The cd command stands for?",
+            "The first ___ always represents the root directory."
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"*","\"\"","$","#"},
+            {"Created directory","Current directory","Parent directory","Change directory"},
+            {"Complete directory","Created directory","Change directory","Current directory"},
+            {"– minus","/ slash","* asterisks","+ plus"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "$",
+            "Parent directory",
+            "Change directory",
+            "/ slash"
+    };
+
+}
